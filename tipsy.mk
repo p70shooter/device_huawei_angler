415543918a5396666db4ce4b5c25ddbdc26d5fe9
f100cleveland <dustin.rinne@gmail.com>
2016-11-23 23:45:49 -0600

    Create tipsy.mk

---
+++ tipsy.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := tipsy_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD91K/3318877:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NBD91K 3318877 release-keys"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440

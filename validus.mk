b01c7d01520dfd7c2d288548c1db2bac7456212d
f100cleveland <dustin.rinne@gmail.com>
2016-12-30 13:26:17 -0600

    Update and rename tesla.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -17,17 +17,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
-# Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common.mk)
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+# Inherit some common validus stuff.
+$(call inherit-product, vendor/validus/config/common.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := tesla_angler
+PRODUCT_NAME := validus_angler
 PRODUCT_DEVICE := angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
@@ -38,6 +38,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
     PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440

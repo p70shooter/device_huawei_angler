7dd7fd3ef992807362aee7ce352b62166e43cad5
Dustin Rinne <dustin.rinne@gmail.com>
2017-01-05 20:37:08 -0600

    Update fingerprint to N4F26J
    Change-Id: Ibecb99c8d5e8b42156b7b7ff4fc550f45e9e696d

--- validus.mk
+++ validus.mk
@@ -36,12 +36,11 @@ PRODUCT_MANUFACTURER := Huawei
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
-
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Dustin Rinne(f100cleveland)"
+	DEVICE_MAINTAINERS="Dustin Rinne (f100cleveland)"
